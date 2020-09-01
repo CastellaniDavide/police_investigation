00013c8103c78bc3126172639512ed158cbc3af0
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-01 13:57:55 +0200

    Created bin/template.cpp

---
+++ bin/police_investigation.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file police_investigation.cpp
+ *
+ * @version 01.01 2020-9-1
+ *
+ * @brief https://training.olinfo.it/#/task/ois_police/statement
+ *
+ * @ingroup police_investigation
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+//  uncomment the following lines if you want to read/write from files
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  cin >> N;
+
+  return 0;
+}
